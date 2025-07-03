7411bfb33b64baaa6e22d46abbb1dc23204100ed
turukola <babonekukang12@gmail.com>
2025-07-04 06:02:16 +0700

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { username } = req.query;
 
   if (!username) {
