6d3b40921f9177c182f152566ccb9958cd6721a9
turukola <babonekukang12@gmail.com>
2025-07-04 07:12:57 +0700

    Update key.js

--- api/key.js
+++ api/key.js
@@ -3,17 +3,17 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  const { STORAGE_REST_API_URL, STORAGE_REST_API_TOKEN } = process.env;
+  const {  KV_REST_API_URL, KV_REST_API_TOKEN  } = process.env;
   const key = "password";
 
-  if (!STORAGE_REST_API_URL || !STORAGE_REST_API_TOKEN) {
+  if (!KV_REST_API_URL || !KV_REST_API_TOKEN) {
     return res.status(500).json({ error: "Redis config missing" });
   }
 
   try {
-    const response = await fetch(`${STORAGE_REST_API_URL}/get/${key}`, {
+    const response = await fetch(`${KV_REST_API_URL}/get/${key}`, {
       headers: {
-        Authorization: `Bearer ${STORAGE_REST_API_TOKEN}`
+        Authorization: `Bearer ${KV_REST_API_TOKEN}`
       }
     });
 
